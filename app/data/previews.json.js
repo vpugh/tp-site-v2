e080d43b46f5cf6b5dc9866bd3e238b8b9bc11df
Tori <taws26@gmail.com>
2018-06-12 23:21:03 -0400

    Files Edits, Fixes, Deletions

---
+++ app/data/previews.json.js
@@ -0,0 +1,65 @@
+export default [
+    {
+        worklink: '/work/ucd',
+        img1x: 'images/Thumbnails/updates@1x.jpg',
+        img2x: 'images/Thumbnails/updates@2x.jpg',
+        type: 'Case Study',
+        title: 'UCD Email Series',
+    },
+    {
+        worklink: '/work/cdew-redesign',
+        img1x: 'images/Thumbnails/cdeworld-2@1x.jpg',
+        img2x: 'images/Thumbnails/cdeworld-2@2x.jpg',
+        type: 'Case Study',
+        title: 'CDEWorld Redesign',
+    },
+    {
+        worklink: '/work/pomodoro',
+        img1x: 'images/Thumbnails/pomodoro-thumb.jpg',
+        img2x: 'images/Thumbnails/pomodoro-thumb.jpg',
+        type: 'Github Pages - App',
+        title: 'Pomodoro Timer App',
+    },
+    {
+        worklink: '/work/invoice',
+        img1x: 'images/Thumbnails/invoice-thumb.jpg',
+        img2x: 'images/Thumbnails/invoice-thumb.jpg',
+        type: 'CodePen',
+        title: 'Invoice Time App',
+    },
+    {
+        worklink: '/work/emailgui',
+        img1x: 'images/Thumbnails/emailgui-thumb.jpg',
+        img2x: 'images/Thumbnails/emailgui-thumb.jpg',
+        type: 'CodePen',
+        title: 'Email Workflow GUI',
+    },
+    {
+        worklink: '/work/vpdesigns',
+        img1x: 'images/Thumbnails/vpdesigns-thumb.jpg',
+        img2x: 'images/Thumbnails/vpdesigns-thumb.jpg',
+        type: 'Case Study',
+        title: 'VP-Designs'
+    },
+    {
+        worklink: '/work/uniag',
+        img1x: 'images/Thumbnails/iagny@1x.jpg',
+        img2x: 'images/Thumbnails/iagny@2x.jpg',
+        type: 'Case Study',
+        title: 'United Nations: Inter-Agency Games',
+    },
+    //{
+        //worklink: '/work/interstice',
+        //img1x: 'images/Thumbnails/interstice.jpg',
+        //img2x: 'images/Thumbnails/interstice@1x.jpg',
+        //type: 'Case Study',
+        //title: 'Interstice',
+    //},
+    {
+        worklink: '/work/newsletters',
+        img1x: 'images/Thumbnails/email-templates@1x.jpg',
+        img2x: 'images/Thumbnails/email-templates@2x.jpg',
+        type: 'Case Study',
+        title: 'Email Templates',
+    }
+]
