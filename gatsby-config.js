f4bc9746b4a20340dff44d3acc9ac6576f9d0cc8
vpugh <hello@toripugh.com>
2021-05-22 22:58:33 -0400

    Start from scratch with Gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  siteMetadata: {
+    title: "tp-site-v2",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
