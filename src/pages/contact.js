1903715331bd0fb532510f8d7645fe0c0daa20bb
vpugh <hello@toripugh.com>
2021-09-04 09:48:20 -0400

    Add name to contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -17,7 +17,7 @@ const ContactPage = () => {
             Can always reach out to talk about a project, position, or anything
             design/development related.
           </p>
-          <form method='POST' data-netlify='true' className='contact-form'>
+          <form name='contact' method='POST' netlify className='contact-form'>
             <input type='text' placeholder='Your Name' />
             <input type='email' placeholder='Your Email' />
             <textarea rows='4' placeholder='What did you want to talk about?' />
